af7b2f39ff3a41931b62071b663062722c402e48
VladTapes30 <vladimirn815@gmail.com>
2024-01-21 20:34:53 -0600

    prueba
    prueba

---
+++ PULL_UP_PRUEBA.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PULL_UP_PRUEBA.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PULL_UP_PRUEBA.X.production.hex
